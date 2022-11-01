9970271febf062d5068a2eb8d9b33494f39577ad
sandeepPangare <pangaresandeep96@gmail.com>
2022-11-01 08:40:34 +0530

    Array problems

--- src/OddPosition.java
+++ src/OddPosition.java
@@ -1,7 +1,6 @@
 public class OddPosition {
     public static void main(String[] args) {
-
-        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] arr = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1};
         for (int i =0; i<=arr.length; i++) {
             if (i % 2 != 0) {
                 System.out.print(arr[i]+" ");
---
+++ src/RotateRight.java
@@ -0,0 +1,11 @@
+public class RotateRight {
+    public static void main(String[] args) {
+
+        int [] arr = new int[] {1,2,3,4,5};
+        int n=1;
+
+        for (int i=0; i< arr.length;i++){
+            System.out.println("\t" +arr[i]); //"\t"used for space
+        }
+    }
+}
